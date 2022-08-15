c43100d3477b858fe36462d95cf5b72639dfb102
adnan159 <osmanhaider159@gmail.com>
2022-08-15 23:19:35 +0600

    Total price in cart added

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -13,10 +13,20 @@ const CartItems = ({title, price}) => {
 }
 
 const Cart = ({cart}) => {
+  const total = cart.reduce( ( sum, current ) => sum + current.price, 0 );
+
   return  (
     <div className="cart-bar">
       <h4>Cart Items</h4>
       {cart.map( item => <CartItems {...item} key={item.id}/>)}
+
+      <div className="cart-items">
+        <div className="item-info">
+          <span>Total</span>
+          <span>$ {total}</span>
+        </div>
+      </div>
+
     </div>
   );
 }
